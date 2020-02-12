2ecce9b220b26087f7935ba41897c383f5e87fcb
Robson Oliveira <oliveira.robs@outlook.com>
2020-02-12 07:21:04 -0500

    Folder structure

---
+++ AnimeManga/AnimeManga.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AnimeManga.Model
+{
+    public class Class1
+    {
+    }
+}
---
+++ AnimeManga/AnimeManga.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AnimeManga.Service
+{
+    public class Class1
+    {
+    }
+}
